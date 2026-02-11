b797555f560163705bcb5cf1d82c12e30a952b27
Alif <alifmuhamadramdan75@gmail.com>
2026-02-11 18:57:59 +0700

    update user information

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -25,6 +25,22 @@ class ProfileController extends Controller
     }
 
     /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('success', 'Profil berhasil diperbarui!');
+    }
+
+    /**
      * Update the user's password.
      */
    public function updatePassword(Request $request): RedirectResponse
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="{{ route('profile.edit') }}" id="profileForm">
+<form method="POST" action="{{ route('profile.update') }}" id="profileForm">
     @csrf
     @method('PATCH')
 
